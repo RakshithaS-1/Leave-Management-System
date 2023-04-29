c2875a7658bdc0a6e513a8066b1ddc4b53c25d54
RakshithaS-1 <116474864+RakshithaS-1@users.noreply.github.com>
2023-04-29 21:58:17 +0530

    Update AdminController.java

--- leavetracker/src/main/java/com/example/leavetracker/controller/AdminController.java
+++ leavetracker/src/main/java/com/example/leavetracker/controller/AdminController.java
@@ -8,12 +8,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.*;
+
 @Controller
 public class AdminController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
-
+    
+    
     @GetMapping("/admin")
     public String admin(Model model) {
         model.addAttribute("employees", employeeRepository.findAll());
@@ -21,9 +24,33 @@ public class AdminController {
         return "admin";
     }
 
+    List<Command> commandHistory = new ArrayList<Command>();
     @PostMapping("/admin/addEmployee")
     public String addEmployee(Employee employee) {
-        employeeRepository.save(employee);
+    	Command command = new AddEmployeeCommand(employee);
+        command.execute(employeeRepository);
+        commandHistory.add(command);
         return "redirect:/admin";
     }
+
+    public interface Command {
+        void execute(EmployeeRepository employeeRepository);
+    }
+
+    public class AddEmployeeCommand implements Command {
+
+        private final Employee employee;
+    
+        public AddEmployeeCommand(Employee employee) {
+            this.employee = employee;
+        }
+    
+        @Override
+        public void execute(EmployeeRepository employeeRepository) {
+            employee.setleaveLeft(10);
+            employeeRepository.save(employee);
+        }
+    }
+    
+
 }
